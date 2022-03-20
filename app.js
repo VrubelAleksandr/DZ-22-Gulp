4d35cad5cb06d097ba2473a1362a9d904a1fbbe5
VrubelAleksandr <vrubelaleksandr@gmail.com>
2022-03-20 17:13:30 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function ($) {
+  $("#lightSlider").lightSlider({
+    pause: 5000,
+    item: 1,
+    slideMove: 1,
+    speed: 100,
+    loop: true,
+    auto: true,
+  });
+});
